64c356cd6da9693e033a6acfe160904bb7fef3cd
Egor Shkirya <zuhijan@yandex.ru>
2019-09-24 22:46:41 +0600

    add module api. modif README

--- scripts/script.js
+++ scripts/script.js
@@ -6,25 +6,9 @@
 */
 import CardList from './cardlist.js';
 import Popup from './popup.js';
+import Api from './api.js';
 
 const serverUrl = NODE_ENV === 'development' ? 'http://praktikum.tk/cohort2' : 'https://praktikum.tk/cohort2';
-
-/* class Popup {
-  constructor(popupLayer) {
-    this.popup = popupLayer;
-
-    this.popup.addEventListener("click", event => {
-      if (event.target.classList.contains("popup__close")) this.close();
-    });
-  }
-  open() {
-    this.popup.classList.add("popup_is-opened");
-  }
-  close() {
-    this.popup.classList.remove("popup_is-opened");
-  }
-} */
-
 const options = {
   baseUrl: serverUrl,
   headers: {
@@ -33,56 +17,10 @@ const options = {
   }
 };
 
-class Api {
-  constructor(options) {
-    this.baseUrl = options.baseUrl;
-    this.headers = options.headers;
-  }
-  getResponseJson(res) {
-    if (res.ok) {
-      return res.json();
-    } else {
-      return Promise.reject(res.status);
-    }
-  }
-  getProfileValue() {
-    return fetch(`${this.baseUrl}/users/me`, { headers: this.headers }).then(
-      res => this.getResponseJson(res)
-    );
-  }
-  getInitialCards() {
-    return fetch(`${this.baseUrl}/cards`, { headers: this.headers }).then(res =>
-      this.getResponseJson(res)
-    );
-  }
-  patchProfile(name, about) {    
-    return fetch(`${this.baseUrl}/users/me`, {   
-      method: "PATCH",
-      headers: this.headers,
-      body: JSON.stringify({
-        name: name,
-        about: about
-      })
-    })
-      .then(res => this.getResponseJson(res))        
-  }
-  addCardRequest(name, link) {
-    return fetch(`${this.baseUrl}/cards`, {
-      method: "POST",
-      headers: this.headers,
-      body: JSON.stringify({
-        name: name,
-        link: link
-      })
-    })
-      .then(res => this.getResponseJson(res))      
-  }
-}
-
 const root = document.querySelector(".root");
 const popup = root.querySelector(".popup");
-const popupEdit = root.querySelector(".popup-edit"); 
-const popupImage = root.querySelector(".popup-image"); 
+const popupEdit = root.querySelector(".popup-edit");
+const popupImage = root.querySelector(".popup-image");
 const form = document.forms.new;
 const buttonAdd = form.elements.button;
 const title = form.elements.title;
@@ -94,7 +32,7 @@ const popupEditInputAbout = formEdit.elements.about;
 const userInfoName = root.querySelector(".user-info__name");
 const userInfoJob = root.querySelector(".user-info__job");
 const userInfoPhoto = root.querySelector(".user-info__photo");
-const popupErrorMessage = popup.querySelectorAll(".error-message"); 
+const popupErrorMessage = popup.querySelectorAll(".error-message");
 const popupEditErrorMessage = popupEdit.querySelectorAll(".error-message");
 const popupCard = new Popup(popup);
 const popupEditProfile = new Popup(popupEdit);
@@ -135,7 +73,7 @@ api
 function profileValue() {
   popupEditInputName.value = userInfoName.textContent;
   popupEditInputAbout.value = userInfoJob.textContent;
-  popupEditErrorMessage[0].textContent = ""; 
+  popupEditErrorMessage[0].textContent = "";
   popupEditErrorMessage[1].textContent = "";
 }
 function validate(event) {
@@ -165,11 +103,11 @@ function validate(event) {
   }
   return;
 }
-root.addEventListener("click", function(event) {
+root.addEventListener("click", function (event) {
   if (event.target.classList.contains("user-info__button")) {
     form.reset();
     popupCard.open();
-    popupErrorMessage[0].textContent = ""; 
+    popupErrorMessage[0].textContent = "";
     popupErrorMessage[1].textContent = "";
     buttonAdd.classList.remove("popup__button_active");
     buttonAdd.setAttribute("disabled", true);
@@ -185,37 +123,37 @@ root.addEventListener("click", function(event) {
     image.src = imageValue[1];
   }
 });
-buttonAdd.addEventListener("click", function() {
+buttonAdd.addEventListener("click", function () {
   api.addCardRequest(title.value, link.value)
-  .then((res) => {
-    console.log(res)
-    cardList.addCard(res.name, res.link)
-    popupCard.close();
-  } )
-  .catch((err) => console.log(err))
-  .finally(() =>  render(false, buttonAdd));
+    .then((res) => {
+      console.log(res)
+      cardList.addCard(res.name, res.link)
+      popupCard.close();
+    })
+    .catch((err) => console.log(err))
+    .finally(() => render(false, buttonAdd));
 
   render(true, buttonAdd);
   form.reset();
 });
-buttonSaveProfile.addEventListener("click", function() {
+buttonSaveProfile.addEventListener("click", function () {
   render(true, buttonSaveProfile);
   api.patchProfile(popupEditInputName.value, popupEditInputAbout.value)
-  .then(() => {
-     userInfoName.textContent = popupEditInputName.value;
-     userInfoJob.textContent = popupEditInputAbout.value;
-     popupEditProfile.close();
-  })
-  .catch((err) => {
-    console.log(err);
-  })
-  .finally(() => {
-  render(false, buttonSaveProfile); 
-  });
+    .then(() => {
+      userInfoName.textContent = popupEditInputName.value;
+      userInfoJob.textContent = popupEditInputAbout.value;
+      popupEditProfile.close();
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+    .finally(() => {
+      render(false, buttonSaveProfile);
+    });
 
-  
+
 });
-form.addEventListener("input", function() {
+form.addEventListener("input", function () {
   if (
     title.value.trim().length <= 1 ||
     title.value.trim().length >= 30 ||
@@ -228,7 +166,7 @@ form.addEventListener("input", function() {
     buttonAdd.removeAttribute("disabled");
   }
 });
-formEdit.addEventListener("input", function() {
+formEdit.addEventListener("input", function () {
   if (
     popupEditInputName.value.trim().length <= 1 ||
     popupEditInputName.value.trim().length > 30 ||
